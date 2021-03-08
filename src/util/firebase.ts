423a6741b630cdd74da33227ac54d253897d9213
강동윤 <kdy1997.dev@gmail.com>
2021-03-08 19:19:42 +0900

    Fix

--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -2,20 +2,24 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/analytics'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBxzHIAi4QHYE_b2z9Npz45FA6Gqvo_gUM",
-    authDomain: "stc-rs.firebaseapp.com",
-    projectId: "stc-rs",
-    storageBucket: "stc-rs.appspot.com",
-    messagingSenderId: "396574604098",
-    appId: "1:396574604098:web:b0d421aa1fb42bcf19a6db",
-    measurementId: "G-8757N7Y0S0"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+if (ExecutionEnvironment.canUseDOM) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBxzHIAi4QHYE_b2z9Npz45FA6Gqvo_gUM",
+        authDomain: "stc-rs.firebaseapp.com",
+        projectId: "stc-rs",
+        storageBucket: "stc-rs.appspot.com",
+        messagingSenderId: "396574604098",
+        appId: "1:396574604098:web:b0d421aa1fb42bcf19a6db",
+        measurementId: "G-8757N7Y0S0"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+
+export const auth = ExecutionEnvironment.canUseDOM ? firebase.auth() : undefined;
+export const firestore = ExecutionEnvironment.canUseDOM ? firebase.firestore() : undefined;
